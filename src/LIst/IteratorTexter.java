38ac35e790d91cb73147052d3f291db84322c30b
‘mxg133’ <396330646@qq.com>
2020-05-13 12:25:41 +0800

    20.5.13

---
+++ src/LIst/IteratorTexter.java
@@ -0,0 +1,23 @@
+package LIst;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class IteratorTexter {
+    public static void main(String[] args) {
+        String num[] = {
+                "123", "1234", "3543", "4365", "235235", "23523", "23", "2345", "42345", "234"
+        };
+        Vector<String> aVector = new Vector<String>(java.util.Arrays.asList(num));
+        System.out.println("Before Vector: " + aVector);
+        Iterator<String> nums = aVector.iterator();
+        while (nums.hasNext()){
+            String aString = (String)nums.next();
+            System.out.println(aString);
+            if (aString.length() > 4){
+                nums.remove();
+            }
+        }
+        System.out.println("After Vector: " + aVector);
+    }
+}
