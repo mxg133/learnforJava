751dbb5491d598da1b0f2972c95b181e33440bea
‘mxg133’ <396330646@qq.com>
2020-05-15 11:23:33 +0800

    day02 20.5.15 11:23

---
+++ src/atguigu/day02/text4.java
@@ -0,0 +1,22 @@
+package atguigu.day02;
+
+public class text4 {
+    public static void main(String[] args) {
+        //默认即int
+        long l = 123213;
+        System.out.println(l);
+        //加个l 就是long
+        long l1 = 123213234543434343l;
+        System.out.println(l1);
+
+//        必须加f
+        float f1 = 12.3f;
+
+//        整形常量 默认为int
+//        浮点型常量 默认为double
+        byte b = 12;
+//        byte b1 = b + 1;
+
+//        float f1 = b + 12.3;
+    }
+}
