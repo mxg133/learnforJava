1297ab60e28711e0eae552582ecfa598efbc37eb
‘mxg133’ <396330646@qq.com>
2020-05-19 12:37:06 +0800

    2020.5.19 12：37

---
+++ src/atguigu/myText/myText01/myText.java
@@ -0,0 +1,9 @@
+package atguigu.myText.myText01;
+
+public class myText {
+    public static void main(String[] args) {
+        if((1==0) & (2>0)){
+            System.out.println("1111");
+        }
+    }
+}
