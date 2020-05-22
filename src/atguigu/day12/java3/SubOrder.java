c5d5d4e766a87997f690c6ca82639401514ff430
‘mxg133’ <396330646@qq.com>
2020-05-22 12:10:04 +0800

    2020.5.22 12：09

---
+++ src/atguigu/day12/java3/SubOrder.java
@@ -0,0 +1,23 @@
+//package atguigu.day12.java3;
+//
+//
+//import atguigu.day10.java.Order;
+//
+//public class SubOrder extends Order {
+//
+//	public void method(){
+//		orderProtected = 1;
+//		orderPublic = 2;
+//
+//		methodProtected();
+//		methodPublic();
+//
+//		//在不同包的子类中，不能调用Order类中声明为private和缺省权限的属性、方法
+////		orderDefault = 3;
+////		orderPrivate = 4;
+////
+////		methodDefault();
+////		methodPrivate();
+//	}
+//
+//}
