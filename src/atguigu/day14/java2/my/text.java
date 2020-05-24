2e9f016b082cc49adb88d6f57b9f6fcc6e85c894
‘mxg133’ <396330646@qq.com>
2020-05-24 16:06:22 +0800

    2020.5.23 16:06

---
+++ src/atguigu/day14/java2/my/text.java
@@ -0,0 +1,13 @@
+package atguigu.day14.java2.my;
+
+public class text {
+//    Bank bank1 = Bank.getInstance();
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+
+        }
+    }
+}
