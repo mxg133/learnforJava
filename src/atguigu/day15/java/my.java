8720cb651c5d6239c49f511243e64b33486b27f8
‘mxg133’ <396330646@qq.com>
2020-05-25 12:52:27 +0800

    2020.5.25 12:50

---
+++ src/atguigu/day15/java/my.java
@@ -0,0 +1,9 @@
+package atguigu.day15.java;
+
+public class my {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        int a = 1;
+        System.out.println(System.currentTimeMillis());
+    }
+}
